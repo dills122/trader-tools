05e689c239d67853f75639637117b0a76f841577
Dylan Steele <dylansteele57@gmail.com>
2021-01-18 00:57:19 -0500

    fixed bug with async iterator

--- lib/trader-sdk/lib/util.ts
+++ lib/trader-sdk/lib/util.ts
@@ -4,3 +4,9 @@ import watchlist from './watchlist.config';
 export const getWatchlist = (): string[] => {
     return _.keys(watchlist);
 }
+
+export const asyncFilter = async (arr, predicate) => {
+    const results = await Promise.all(arr.map(predicate));
+
+    return arr.filter((_v, index) => results[index]);
+};
--- services/trading-services/lib/52week.service.ts
+++ services/trading-services/lib/52week.service.ts
@@ -3,8 +3,7 @@ import { util, Emailer } from 'trader-sdk';
 
 export const service = async () => {
     try {
-        //TODO test more
-        const results = await Promise.all(util.getWatchlist().filter(async (symbol) => {
+        const results = await util.asyncFilter(util.getWatchlist(), async (symbol) => {
             const { week52Low, latestPrice } = await QuouteService.quote(symbol);
             console.log(week52Low, latestPrice);
             if (week52Low >= latestPrice) {
@@ -12,7 +11,7 @@ export const service = async () => {
             }
             const percentDifference = (Math.abs(week52Low - latestPrice) / ((latestPrice + week52Low) / 2)) * 100;
             return percentDifference <= 10;
-        }));
+        });
         if (results.length <= 0) {
             return;
         }
@@ -25,9 +24,3 @@ export const service = async () => {
         console.log(err);
     }
 };
-//TODO remove after testing
-export default service;
-
-(async () => {
-    await service();
-})();

5ca35922edffaf5cf304ce498a33e67acb0e6334
Dylan Steele <dylansteele57@gmail.com>
2021-01-18 19:56:33 -0500

    started bb service

---
+++ lib/trader-sdk/lib/bollingerBands/index.ts
@@ -0,0 +1,9 @@
+import * as BB from './bb';
+
+export default {
+    BB
+};
+
+export {
+    BB
+};
--- lib/trader-sdk/lib/util.ts
+++ lib/trader-sdk/lib/util.ts
@@ -10,3 +10,14 @@ export const asyncFilter = async (arr, predicate) => {
 
     return arr.filter((_v, index) => results[index]);
 };
+
+export const calculatePercentType = (arry: boolean[], bit: boolean) => {
+    const recordCount = arry.length;
+    let total = 0;
+    _.each(arry, (value) => {
+        if (value === bit) {
+            total += 1;
+        }
+    });
+    return _.round(total / recordCount * 100, 2);
+};

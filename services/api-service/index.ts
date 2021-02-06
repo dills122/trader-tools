0583353e0a26959218cf14096b75937b17f8f7e2
Dylan Steele <dylansteele57@gmail.com>
2021-02-06 17:26:33 -0500

    created historic batch price endpoint

--- services/api-service/index.ts
+++ services/api-service/index.ts
@@ -1,15 +1,18 @@
 import IEXCloud from './lib/iexcloud.service';
 import * as QuouteService from './lib/quote.service';
 import * as HistoricPrices from './lib/historic-prices.service';
+import * as HistoricBatchPrices from './lib/historic-batch-chart.service';
 
 export default {
     IEXCloud,
     QuouteService,
-    HistoricPrices
+    HistoricPrices,
+    HistoricBatchPrices
 };
 
 export {
     IEXCloud,
     QuouteService,
-    HistoricPrices
+    HistoricPrices,
+    HistoricBatchPrices
 };

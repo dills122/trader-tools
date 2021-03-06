c6b48d56542c96f0d2658d1211123e3d4f81a83e
Matthew Hudock <matthewd1715@gmail.com>
2021-03-06 15:28:08 -0500

    Created The Crypto Symbol Service + Test

---
+++ services/api-service/lib/crypto-symbols.service.ts
@@ -0,0 +1,17 @@
+import IEXCloud from './iexcloud.service';
+
+export interface CryptoSymbolsData {
+    symbol: string,
+    name: string,
+    exchange: string,
+    iexId: string,
+    currency: string,
+    date: string,
+    type: string,
+    isEnabled: boolean,
+    region: string
+};
+
+export const cryptoSymbols = async () => {
+    return await IEXCloud<CryptoSymbolsData>('/ref-data/crypto/symbols');
+};

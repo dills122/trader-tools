09163f44427e059713bf03b29db71c3708f80743
Dylan Steele <dylansteele57@gmail.com>
2021-08-09 19:39:33 -0400

    setup testing deps, generic test

--- apps/sentiment-rpc-server/src/rpc/generic.rpc.ts
+++ apps/sentiment-rpc-server/src/rpc/generic.rpc.ts
@@ -5,10 +5,10 @@ import config from '../proto.config';
 const { GenericSentimentService } = config.Services;
 import { ProtoGrpcType } from '../../generated/generic';
 import { GenericSentimentServiceClient } from '../../generated/generic/GenericSentimentService';
-import { AnalysisRequest } from '../../generated/generic/AnalysisRequest';
+import { AnalysisRequest__Output } from '../../generated/generic/AnalysisRequest';
 import { SentimentAnalysisResult__Output } from '../../generated/generic/SentimentAnalysisResult';
 
-export default (args: AnalysisRequest): Promise<SentimentAnalysisResult__Output[]> => {
+export default (args: AnalysisRequest__Output): Promise<SentimentAnalysisResult__Output[]> => {
   const rpcClient = grpcTools.clientFactory<ProtoGrpcType, GenericSentimentServiceClient>(
     GenericSentimentService
   );
---
+++ apps/sentiment-rpc-server/test/rpc/generic.spec.ts
@@ -0,0 +1,74 @@
+import grpcTools from 'grpc-tools';
+import { describe } from 'mocha';
+import { expect } from 'chai';
+import Sinon from 'sinon';
+import GenericRPC from '../../src/rpc/generic.rpc';
+
+const subreddit = 'wallstreetbets';
+
+describe('RPC::', function () {
+  describe('RPC::', function () {
+    let sandbox: Sinon.SinonSandbox;
+    const stubs: any = {};
+
+    beforeEach(() => {
+      sandbox = Sinon.createSandbox();
+      stubs.waitForReadyStub = sandbox.stub().callsArg(1);
+      stubs.AnalyzeStub = sandbox.stub().callsArgWith(1, null, {
+        analysisResults: []
+      });
+      stubs.clientFactoryStub = sandbox.stub(grpcTools, 'clientFactory').returns({
+        waitForReady: stubs.waitForReadyStub,
+        Analyze: stubs.AnalyzeStub
+      });
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('Should resolve if no error returned', async () => {
+      const results = await GenericRPC({
+        analyzer: 'natural',
+        serviceAnalysisType: 'front-page',
+        socialSource: 'reddit',
+        subreddit: subreddit
+      });
+      expect(stubs.waitForReadyStub.callCount).to.equal(1);
+      expect(stubs.AnalyzeStub.callCount).to.equal(1);
+      expect(results).to.be.an('array').and.have.length(0);
+    });
+    it('Should fail if wait returns error', async () => {
+      stubs.waitForReadyStub.callsArgWith(1, Error('ERR'));
+      try {
+        const results = await GenericRPC({
+          analyzer: 'natural',
+          serviceAnalysisType: 'front-page',
+          socialSource: 'reddit',
+          subreddit: subreddit
+        });
+        expect(results).to.be.not.undefined;
+      } catch (err) {
+        expect(err.message).to.equal('Client connection error');
+        expect(stubs.waitForReadyStub.callCount).to.equal(1);
+        expect(stubs.AnalyzeStub.callCount).to.equal(0);
+      }
+    });
+    it('Should fail if Analyze returns error', async () => {
+      stubs.AnalyzeStub.callsArgWith(1, Error('ERR'));
+      try {
+        const results = await GenericRPC({
+          analyzer: 'natural',
+          serviceAnalysisType: 'front-page',
+          socialSource: 'reddit',
+          subreddit: subreddit
+        });
+        expect(results).to.be.not.undefined;
+      } catch (err) {
+        expect(err.message).to.equal('ERR');
+        expect(stubs.waitForReadyStub.callCount).to.equal(1);
+        expect(stubs.AnalyzeStub.callCount).to.equal(1);
+      }
+    });
+  });
+});

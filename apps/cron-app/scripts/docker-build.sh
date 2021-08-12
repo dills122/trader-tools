db3b2e79a7682dcc264ce050b3ca15c67a451345
Dylan Steele <dylansteele57@gmail.com>
2021-08-12 19:13:45 -0400

    fixed docker setup, spun up first full env

--- apps/cron-app/scripts/docker-build.sh
+++ apps/cron-app/scripts/docker-build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-npm install -g @microsoft/rush
-rush unlink
 rm -rf common/temp
-rush update --purge
 rush install
 rush build
 
-mkdir ../prod
-rush deploy --project cron-app --target-folder ../prod
+mkdir ./prod
+rush deploy --project cron-app --target-folder ./prod

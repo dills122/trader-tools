8a2122fb968559377b903988efdfde1983cf636b
Dylan Steele <dylansteele57@gmail.com>
2021-09-20 21:44:43 -0400

    fixed linting issues

---
+++ scripts/docker-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apk add --update bash && rm -rf /var/cache/apk/*
+adduser -S app
+rm -rf ./common/temp
+rush update --purge
+rush build
+mkdir ./prod
+rush deploy --project $1 --target-folder ./prod
+cp ./.env ./prod/

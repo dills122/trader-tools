c9b67bf9b80d2892052e575c6d3c03bc58321242
Dylan Steele <dylansteele57@gmail.com>
2022-02-17 20:08:47 -0500

    updated docker files for better builds, fixed pm2 configs

--- lib/is-ticker-symbol/lib/runner.ts
+++ lib/is-ticker-symbol/lib/runner.ts
@@ -1,63 +1,64 @@
-import { RebaseTickerList } from './rebase-ticker-list';
-import meow from 'meow';
+//TODO update to use not es module
+// import { RebaseTickerList } from './rebase-ticker-list';
+// import meow from 'meow';
 
-const cli = meow(
-  `
-	Usage
-	  $ execute
+// const cli = meow(
+//   `
+// 	Usage
+// 	  $ execute
 
-	Options
-	  --exclude, -x  exclude specific sources
-      --filter, -f type of tickers you want
+// 	Options
+// 	  --exclude, -x  exclude specific sources
+//       --filter, -f type of tickers you want
 
-	Examples
-	  $ execute
-	  
-`,
-  {
-    booleanDefault: undefined,
-    flags: {
-      exclude: {
-        type: 'string',
-        alias: 'x'
-      },
-      filter: {
-        type: 'string',
-        alias: 'f'
-      }
-    }
-  }
-);
+// 	Examples
+// 	  $ execute
 
-const checkCLIArgs = () => {
-  const cmd = cli.input[0];
-  const isExecute = cmd === 'execute';
-  if (cmd && !isExecute) {
-    throw Error('Incorrect command given');
-  }
-  const flags = cli.flags;
-  const excludedSources = flags['exclude'] || '';
-  return {
-    excludedSources: excludedSources.split(','),
-    filterType: flags.filter
-  };
-};
+// `,
+//   {
+//     booleanDefault: undefined,
+//     flags: {
+//       exclude: {
+//         type: 'string',
+//         alias: 'x'
+//       },
+//       filter: {
+//         type: 'string',
+//         alias: 'f'
+//       }
+//     }
+//   }
+// );
 
-(async () => {
-  try {
-    const args = checkCLIArgs();
-    const RebaseInst = new RebaseTickerList({
-      excludedSources: args.excludedSources.length > 0 ? args.excludedSources : undefined,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      filterType: <any>args.filterType
-    });
-    console.log('Starting Rebase Service');
-    await RebaseInst.rebase();
-    console.log('Rebase Service Finished Successfully');
-    process.exit(0);
-  } catch (err) {
-    console.error('Rebase Service Finished With Errors');
-    console.error(err);
-    process.exit(1);
-  }
-})();
+// const checkCLIArgs = () => {
+//   const cmd = cli.input[0];
+//   const isExecute = cmd === 'execute';
+//   if (cmd && !isExecute) {
+//     throw Error('Incorrect command given');
+//   }
+//   const flags = cli.flags;
+//   const excludedSources = flags['exclude'] || '';
+//   return {
+//     excludedSources: excludedSources.split(','),
+//     filterType: flags.filter
+//   };
+// };
+
+// (async () => {
+//   try {
+//     const args = checkCLIArgs();
+//     const RebaseInst = new RebaseTickerList({
+//       excludedSources: args.excludedSources.length > 0 ? args.excludedSources : undefined,
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       filterType: <any>args.filterType
+//     });
+//     console.log('Starting Rebase Service');
+//     await RebaseInst.rebase();
+//     console.log('Rebase Service Finished Successfully');
+//     process.exit(0);
+//   } catch (err) {
+//     console.error('Rebase Service Finished With Errors');
+//     console.error(err);
+//     process.exit(1);
+//   }
+// })();
